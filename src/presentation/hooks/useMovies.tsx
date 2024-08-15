98a5387e7fb04f07a02abda9bcfbe9491be30c8c
nicovidal <nico.vidal.m@gmail.com>
2024-08-15 17:41:39 -0400

    AppMovie

---
+++ src/presentation/hooks/useMovies.tsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react"
+import { Movie } from "../../core/entities/movie.entity"
+import * as UseCases from '../../core/use-cases/'
+import { movieDBFetcher } from "../../config/adapters/movieDB.adapter"
+
+export const useMovies = () => {
+
+   const [isLoading, setIsLoading] = useState(true)
+  const [nowPlaying, setNowPlaying] = useState<Movie[]>([])
+  const [popular, setPopular] = useState<Movie[]>([])
+  const [topRated, setTopRated] = useState<Movie[]>([])
+  const [upcoming, setUpcoming] = useState<Movie[]>([])
+
+
+  useEffect(() => {
+
+    initialLoad();
+  }, [])
+  
+
+  const initialLoad=async()=>{
+    const nowPlayingPromise=await UseCases.moviesNowPlayingUseCase(movieDBFetcher)
+    const popularPromise=await UseCases.moviesPopularUseCase(movieDBFetcher)
+    const topRatedPromise=await UseCases.moviesTopRatedUseCase(movieDBFetcher)
+    const upcomingPromise=await UseCases.moviesUpcomingPlayingUseCase(movieDBFetcher)
+
+    const [
+      nowPlayingMovies,
+      popularMovies,
+      topRatedMovies,
+      upcomingMovies
+    ]=await Promise.all([
+      nowPlayingPromise,
+      popularPromise,
+      topRatedPromise,
+      upcomingPromise])
+
+      setNowPlaying(nowPlayingMovies)
+      setPopular(popularMovies)
+      setTopRated(topRatedMovies)
+      setUpcoming(upcomingMovies)
+
+      setIsLoading(false)
+
+      
+
+  }
+
+  return {
+    isLoading,
+    nowPlaying,
+    popular,
+    topRated,
+    upcoming
+
+
+
+  }
+}

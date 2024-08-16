9e6a9ea9f3a05afd71b245287a0170d99b231201
nicovidal <nico.vidal.m@gmail.com>
2024-08-15 21:41:20 -0400

    cast ok

--- src/config/adapters/movieDB.adapter.ts
+++ src/config/adapters/movieDB.adapter.ts
@@ -5,6 +5,6 @@ export const movieDBFetcher=new AxiosAdapter({
     baseUrl:'https://api.themoviedb.org/3/movie/',
     params:{
         api_key:'623051f2d09d4186eedeb1d210c4117e',
-        lenguage:'es'
+        language:'es'
     }
 })
